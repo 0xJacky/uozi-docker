b5d8b11f309f003cc37052bd97fa54431f4f8e08
0xJacky <me@jackyu.cn>
2022-08-05 15:33:05 +0800

    update nginx-ui-base

---
+++ nginx-ui-base/READM.md
@@ -0,0 +1,8 @@
+# nginx ui base
+
+## Base Image
+
+nginx:latest
+
+## Optimize
+1. Use `http://mirrors.aliyun.com/` as apt-get mirror sources
--- nginx-ui-demo-debian-base-slim/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker build -t nginx-ui-demo-debian-base-slim --no-cache .
-docker tag nginx-ui-demo-debian-base-slim uozi/nginx-ui-demo-debian-base-slim
-docker push uozi/nginx-ui-demo-debian-base-slim
-
-docker tag nginx-ui-demo-debian-base-slim registry.cn-shenzhen.aliyuncs.com/uozi/nginx-ui-demo-debian-base-slim
-docker push registry.cn-shenzhen.aliyuncs.com/uozi/nginx-ui-demo-debian-base-slim
