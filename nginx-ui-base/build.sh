1399331018cd4217de571238623f1ac41cbcd1d0
Jacky <me@jackyu.cn>
2024-05-05 19:03:39 +0800

    feat(nginx-ui-base): init config

--- nginx-ui-base/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker build -t nginx-ui-base --no-cache .
-docker tag nginx-ui-base uozi/nginx-ui-base
-docker push uozi/nginx-ui-base
-
-docker tag nginx-ui-base registry.cn-shenzhen.aliyuncs.com/uozi/nginx-ui-base
-docker push registry.cn-shenzhen.aliyuncs.com/uozi/nginx-ui-base
